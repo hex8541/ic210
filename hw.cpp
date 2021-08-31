d3e769c801c05bdb14f50bf444a0004297f63267
hex8541 <m242628@usna.edu>
2021-08-31 17:42:33 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -1,28 +1,17 @@
+//Nicholas Heil, 242628
+
 #include <iostream>
 
 using namespace std;
 
+//Read three letter word in uppercase and print out the same word lowercase.
 int main()
 {
-  int feet1;
-  int inches1;
-  int feet2;
-  int inches2;
-  string junk; //string junk is used to separate ' and " from user input
-  //The following process takes user input and separates it into 
-  cout << "Enter two lengths in feet and inches (larger first!)" << endl;
-  cin >> feet1;
-  cin >> junk;
-  cin >> inches1;
-  cin >> junk; 
-  cin >> feet2;
-  cin >> junk;
-  cin >> inches2;
-  int total1 = (feet1*12) + inches1; //total variables convert ft -> in
-  int total2 = (feet2*12) + inches2;
-  int difference = total1 - total2; //operation performed on total inches
-  int ft = difference/12;
-  int in = difference%12;
-  cout << "Difference is " << ft << "\' " << in << "\'\'" << endl;
-  return 0;
+  //Ask for 3-letter word in all caps
+  cout << "Input a 3-letter word in uppercase letters:";
+  //Read the 3-letter word and assign letters to variables 
+  char c1, c2, c3;
+  cin >> c1 >> c2 >> c3;
+  //Convert letters to lowercase equivalents from ASCII table values 
+  cout << "Output: " << char(c1 + 32) << char(c2 + 32) << char(c3 + 32) << endl;
 }

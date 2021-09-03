012b281dd16ce49f6f8619dea5d5d7a47de26330
hex8541 <m242628@usna.edu>
2021-09-03 08:13:19 -0400

    msg

--- lab02/p3.cpp
+++ lab02/p3.cpp
@@ -13,7 +13,28 @@ int main()
   cin >> user >> group >> other;
 
   //Separate each number from the triple into its bits.
-  user
+  int x, u1, u2, u3, g1, g2, g3, f1, f2, f3;
+  x = user-48;  //x used for calculations
+  
+  //Perform calculations for user
+  u1 = x/4, x = x%4;
+  u2 = x/2, x = x%2;
+  u3 = x/1;
+  cout << "\nUser: \nread " << u1 << "\nwrite " << u2 << "\nexecute " << u3 << endl;
+
+  //Perform calculations for group
+  x = group-48;  //variable -48 is used to cancel out ASCII values of characters
+  g1 = x/4, x = x%4;
+  g2 = x/2, x = x%2;
+  g3 = x/1;
+  cout << "\nGroup: \nread " << g1 << "\nwrite " << g2 << "\nexecute " << g3 << endl;
+
+  //Perform calculations for other (variable f for final)
+  x = other-48;
+  f1 = x/4, x = x%4;
+  f2 = x/2, x = x%2;
+  f3 = x/1;
+  cout << "\nOther: \nread " << f1 << "\nwrite " << f2 << "\nexecute " << f3 << endl;
 }
 
 

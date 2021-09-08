d978d2e23eb4569fafba4ab1c972c33d8d4d4a8a
hex8541 <m242628@usna.edu>
2021-09-08 00:17:35 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -1,37 +1,53 @@
 //Nicholas Heil, 242628
-//Determine if a year is a leap year.
+//Receive two numbers and perform a command that is received separately on one
+//of those numbers.
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-  //Gather input and assign it to variable year.
-  cout << "Enter year: ";
-  int year;
-  cin >> year;
-  //Starts by determining if a year divisible by 100 is a leap year
-  if (year%100 == 0)
-  {
-    if (year%400 == 0)
-    {
-      cout << "Is Leap Year" << endl;
-      return (0);  //Stop program from running next if statement
+  //Gather values for a and b
+  double a, b;
+  char j;  //j used to sort out comma 
+  cout << "Value for a and b?" << endl;
+  cin >> a >> j >> b;
+
+  //Determine command to be executed
+  cout << "What do you want?" << endl; 
+  string c, extra;  //Sorting commands
+  double num;  //value for addition
+  char t;  //target
+  cin >> c;
+
+  //Addition operations
+  if (c == "add"){
+    cin >> num >> extra >> t;
+    if (t == 'a') {  //Addition to a
+    a = (a + num);
+    cout << "a = " << a << " and b = " << b;
     }
     else
     {
-      cout << "Is Not Leap Year" << endl; 
-      return (0); 
+      if (c == "add" && t == 'b'){
+        b = (b + num);
+        cout << "a = " << a << " and b = " << b;
+      }
     }
   }
-  //Next determines if a year not divisible by 100 is a leap year
-  if ((year%4 == 0) && (year%100 != 0))
-  {
-     cout << "Is Leap Year" << endl;
-  }
-  else
-  {
-    cout << "Is Not Leap Year" << endl;
-  }
+  else 
+    {
+    cin >> t;  
+    if (c == "square" && t == 'a'){
+      a = a * a;
+      cout << "a = " << a << " and b = " << b;
+    }
+    else {
+          b = b * b;
+          cout << "a = " << a << " and b = " << b;
+        }
+    }
 }
+

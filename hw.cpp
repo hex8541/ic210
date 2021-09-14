7c29d7e75f039bde2f78636953eb642fbcd041bc
hex8541 <m242628@usna.edu>
2021-09-13 23:21:19 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -1,27 +1,38 @@
 //Nicholas Heil 242628
-//Generate numbers based on set conditions and four provided integers
+//Determine if adult or child based on birthday from text file.
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
 int main()
-{
-  //Assign variables from input
-  int s, e, a, b, t;  //t is test variable
-  cout << "Enter four integers: " << endl;
-  cin >> s >> e >> a >> b;
-  t = s;
-  while (t <= e && t >= s)
-  {
-    if ((t % a) == 0 && (t % b) ==0) //test if number is a multiple of a and b
-    {
-      cout << t << " ";
-      t++;
+{ 
+  //Gather input and setup
+  string file, junk;
+  int adult, children, mm, dd, yy;
+  mm = 0, dd = 0, yy = 0;  //Initialize variables
+  adult = 0, children = 0;
+  char j;
+  cout << "Filename: ";
+  cin >> file;
+  ifstream fin(file);
+
+  //Use loop to sort through all data in file
+  while(fin){
+    cin >> junk >> mm >> j >> dd >> j >> yy;
+    if(yy >= 22){
+      adult = adult + 1;
     }
-    else
-      t++;  //if not, just move on to next number
+    else if (yy == 0 && mm >= 9 && dd >= 27){ //Possible issues could arise from 0
+      adult = adult + 1;
+    }
+      else 
+        children = children + 1;
   }
+  //Output 
+  cout << adult << " adults and " << children << " children" << endl;
 }
 
 

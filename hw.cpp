ba126a423a869b46c7f9bfb7a0ecf21abfd03f78
hex8541 <m242628@usna.edu>
2021-09-14 00:12:19 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -11,21 +11,22 @@ int main()
 { 
   //Gather input and setup
   string file, junk;
-  int adult, children, mm, dd, yy;
-  mm = 0, dd = 0, yy = 0;  //Initialize variables
+  int adult, children, mm, dd;
+  char y1, y2;
+  mm = 0, dd = 0;  //Initialize variables
   adult = 0, children = 0;
   char j;
   cout << "Filename: ";
   cin >> file;
-  ifstream fin(file);
+  ifstream fin("file");
 
   //Use loop to sort through all data in file
   while(fin){
-    cin >> junk >> mm >> j >> dd >> j >> yy;
-    if(yy >= 22){
+    cin >> junk >> mm >> j >> dd >> j >> y1 >> y2;
+    if(y1 >= '2' && y2 >= '2'){
       adult = adult + 1;
     }
-    else if (yy == 0 && mm >= 9 && dd >= 27){ //Possible issues could arise from 0
+    else if (y1 == '0' && y2 == '0' && mm >= 9 && dd >= 27){ 
       adult = adult + 1;
     }
       else 

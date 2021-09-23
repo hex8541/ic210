13b4fd369914dd8afe4a1f10f16ed49eab5fefb3
hex8541 <m242628@usna.edu>
2021-09-23 15:13:22 -0400

    msg

--- rowshift.cpp
+++ rowshift.cpp
@@ -36,33 +36,55 @@ int main ()
   int r = 0, g = 0, b = 0, r2 = 0, g2 = 0, b2 = 0;
   fin1 >> header >> w >> h >> max;  //Read unique values separately
   fin2 >> header >> w2 >> h2 >> max2;  //Read info for background img
+  int cRow = (rs + h);  //Find cutoff row, after which only fout is background
 
-  //Check that images are the same size
-  if (w != w2 && h != h2){
-    cout << "Error: Images have different sizes";
-    return 1;
+  //Determine if rowshift is too large
+  if (cRow > h2){
+    cout << "Error: The foreground goes past the background";
+    exit(1);
   }
-  //Outputs file header
-  fout << header << endl << w << " " << h << endl << max << endl;
   
-  while (fin1){
-    //Cycle through each row
-    while (row < h){
-      if (col < w){  //For each row, work through each column one by one
-        fin1 >> r >> g >> b;
-        fin2 >> r2 >> g2 >> b2;
-        if (r == 0 && g == 255 && b ==0)  //Green background
-          fout << r2 << " " << g2 << " " << b2 << " ";
-        else  //Output non-green image
-          fout << r << " " << g << " " << b << " ";
-        col++;
-      }
-      else {  //When each row finishes, reset col and advance row
-        col = 0;
-        row++;
-      }
+  //Outputs file header
+  fout << header << endl << w2 << " " << h2 << endl << max2 << endl;
+
+  
+  //Output pixels from background equal to rows shifted
+  for (row; row < rs; row++){
+    while (col < w2){
+      fin2 >> r2 >> g2 >> b2;  //Read from background file
+      fout << r2 << " " << g2 << " " << b2 << " ";  //Output to new image
+      col++;
     }
-    cout << "Image saved to " << out << endl;
-    return 0;
+    col = 0;
   }
+
+  for (row; row < cRow; row++){  //Continues until cutoff is reached
+    while (col < w){  //Greenscreen processing for primary image
+      fin1 >> r >> g >> b;
+      fin2 >> r2 >> g2 >> b2;
+      if (r == 0 && g == 255 && b ==0)  //Green background
+          fout << r2 << " " << g2 << " " << b2 << " ";
+      else  //Output non-green image
+          fout << r << " " << g << " " << b << " ";
+      col++; 
+    }
+    while (col < w2){  //Output remainder of row from background
+      fin2 >> r2 >> g2 >> b2;
+      fout << r2 << " " << g2 << " " << b2 << " ";
+      col++;
+    }
+    col = 0;
+  }
+  
+  //Output the remainder of the background file image
+  for (row; row < h2; row++){
+    while (col < w2){
+      fin2 >> r2 >> g2 >> b2;  //Read from background file
+      fout << r2 << " " << g2 << " " << b2 << " ";  //Output to new image
+      col++;
+    }
+    col = 0;
+  }
+  cout << "Image saved to " << out << endl;
+  return 0;
 }

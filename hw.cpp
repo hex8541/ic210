d1f742f1e45b20e7245e5ecd0c81108c4bc4273c
hex8541 <m242628@usna.edu>
2021-10-27 00:44:17 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -1,53 +1,58 @@
 //Nicholas Heil 242628
-//Perform operations on a matrix of integers
+//Sort ten ints and return them in ascending order, odds then evens
 
 #include <iostream>
 
 using namespace std;
 
+void selection_sort(int* A, int n);
+bool before(int a, int b);
+
 int main()
 {
-  //Initialize size of array 
-  cout << "What size? ";
-  int n, m; //n is number of rows, m is number of columns
-  char j;
-  cin >> n >> j >> m;
+  //Create array and gather input from user
+  int* PosInt = new int[10];
+  for (int i=0; i < 10; i++)
+    cin >> PosInt[i];
   
-  //Reads in values for each storage location in the matrix
-  int** A = new int*[n];
-  for (int i=0; i < n; i++){  //Works on array of arrays
-    A[i] = new int[m];  
-    for(int j=0; j < m; j++)  //Inputs values to each location in each array
-     cin >> A[i][j]; 
-  }
+  //Sort
+  selection_sort(PosInt, 10);
   
-  //Find rows whose sum is divisible by 3, or the row is a multiple of 3
-  cout << "rows: ";
-  int sum=0;
-  for (int i=0; i < n; i++){  //Cycles rows to test each
-    for (int j=0; j < m; j++){  //Takes sum of the row
-      sum = sum + A[i][j];
-    }
-    if (sum%3 == 0)  //Checks if sum is divisible evenly by 3
-      cout << i << " ";
-    sum = 0;  //Reset sum for repeated use
-  }
+  //Output
+  for (int i=0; i < 10; i++)
+    cout << PosInt[i] << " ";
 
-  //Find columns whose sum is divisible by 3
-  sum = 0;
-  cout << "\ncols: ";
-  for (int i=0; i < m; i++){  //similar to above, but order reversed/cols first
-    for (int j=0; j < n; j++){  //take sum of current column
-      sum = sum + A[j][i];
-    }
-    if (sum%3 == 0)
-      cout << i << " ";
-    sum = 0;
-  }
 
-  for(int i = 0; i < n; i++)
-    delete [] A[i];
-  delete [] A;  //Delete the array used 
-
+  delete [] PosInt;
   return 0;
 }
+
+void selection_sort(int* A, int n) 
+{
+  for (int i = 0; i < n - 1; ++i) {
+    // find nexti, the index of the next element
+    int nexti = i;
+    for (int j = i + 1; j < n; ++j) {
+      if (before(A[j], A[nexti])) {
+        nexti = j;
+      }
+    }
+    // swap A[i] and A[nexti]
+    int temp = A[i];
+    A[i] = A[nexti];
+    A[nexti] = temp;
+  }
+}
+
+bool before(int a, int b) //Sort by ascending odd, then ascending even
+{
+  if (a%2 == 1 && b%2 ==1)  //If both are odd
+    return a < b;  
+  else if(a%2 == 1)  //If a is odd and b is even, move a up
+    return true;
+  else if(a%2 == 0 && b%2 == 1)  //If a is even and b is odd, move b up
+    return false;
+  else  //If both are even 
+    return a < b;
+}
+

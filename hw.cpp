0c9715db273abfe5e4910b48b4a7b3a178f3ba08
hex8541 <m242628@usna.edu>
2021-11-06 17:06:16 -0400

    msg

--- hw.cpp
+++ hw.cpp
@@ -1,58 +1,79 @@
 //Nicholas Heil 242628
-//Sort ten ints and return them in ascending order, odds then evens
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-void selection_sort(int* A, int n);
-bool before(int a, int b);
+struct Section{
+  string course, schedule;
+  int number;
+};
+
+void searchByCourse(Section* sections, int n);
+void searchBySection(Section* sections, int n);
+Section* read(istream* pin, int* pn);
 
 int main()
 {
-  //Create array and gather input from user
-  int* PosInt = new int[10];
-  for (int i=0; i < 10; i++)
-    cin >> PosInt[i];
-  
-  //Sort
-  selection_sort(PosInt, 10);
-  
-  //Output
-  for (int i=0; i < 10; i++)
-    cout << PosInt[i] << " ";
+  string fname;
+  cout << "Filename: ";
+  cin >> fname;
+  ifstream fin(fname);
+
+  int n;
+  Section* sections = read(&fin, &n);
+
+  string cmd;
+  cout << "> ";
+  while( (cin >> cmd) && (cmd != "quit") )
+  {
+    if( cmd == "course" )
+      searchByCourse(sections, n);
+    else if ( cmd == "section" )
+      searchBySection(sections, n);
+
+    cout << endl << "> ";
+  }
 
 
-  delete [] PosInt;
+  delete [] sections;
   return 0;
 }
 
-void selection_sort(int* A, int n) 
+Section* read(istream* pin, int* pn)
 {
-  for (int i = 0; i < n - 1; ++i) {
-    // find nexti, the index of the next element
-    int nexti = i;
-    for (int j = i + 1; j < n; ++j) {
-      if (before(A[j], A[nexti])) {
-        nexti = j;
-      }
+  char j;
+  *pin >> j >> j >> *pn; //Read in number of sections to main function
+  Section* current = new Section[*pn];
+  for(int i=0; i < *pn; i++){ //read in each element in order
+    *pin >> current[i].course >> current[i].number >> current[i].schedule;
+  }
+  return current;  //returns the pointer to the array
+}
+
+void searchByCourse(Section* sections, int n)
+{
+  string search;
+  cin >> search;
+  for(int i=0; i < n; i++){
+    if(sections[i].course == search){
+      cout << sections[i].course << " " <<  sections[i].number << " "
+        << sections[i].schedule << endl;
     }
-    // swap A[i] and A[nexti]
-    int temp = A[i];
-    A[i] = A[nexti];
-    A[nexti] = temp;
   }
 }
 
-bool before(int a, int b) //Sort by ascending odd, then ascending even
+void searchBySection(Section* sections, int n)
 {
-  if (a%2 == 1 && b%2 ==1)  //If both are odd
-    return a < b;  
-  else if(a%2 == 1)  //If a is odd and b is even, move a up
-    return true;
-  else if(a%2 == 0 && b%2 == 1)  //If a is even and b is odd, move b up
-    return false;
-  else  //If both are even 
-    return a < b;
-}
+  int search;
+  cin >> search;
+  for(int i=0; i < n; i++){
+    if(sections[i].number == search){
+      cout << sections[i].course << " " << sections[i].number << " "
+        << sections[i].schedule << endl;
+    }
+  }
 
+}
